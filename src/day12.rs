7592f8ede106e9a3accce1b34ec8c73d26d99649
owl <17188713+owlCuri@users.noreply.github.com>
2021-12-17 00:48:29 +0800

    Day 12: rewrite using memorization and bitvec

--- src/day12.rs
+++ src/day12.rs
@@ -9,18 +9,24 @@ impl Graph {
         let mut node_cap = vec![false; 2];
         for edge in s.split_ascii_whitespace() {
             let (from, to) = edge.split_once('-').unwrap();
-            if !names.contains_key(from) {
-                names.insert(from, names.len());
-                adj_list.push(Vec::new());
-                node_cap.push(from.as_bytes()[0].is_ascii_uppercase());
-            }
-            let from_idx = *names.get(from).unwrap();
-            if !names.contains_key(to) {
-                names.insert(to, names.len());
-                adj_list.push(Vec::new());
-                node_cap.push(to.as_bytes()[0].is_ascii_uppercase());
-            }
-            let to_idx = *names.get(to).unwrap();
+            let next_idx = names.len();
+            let from_idx = match names.entry(from) {
+                std::collections::hash_map::Entry::Occupied(slot) => *slot.get(),
+                std::collections::hash_map::Entry::Vacant(slot) => {
+                    adj_list.push(Vec::new());
+                    node_cap.push(from.as_bytes()[0].is_ascii_uppercase());
+                    *slot.insert(next_idx)
+                }
+            };
+            let next_idx = names.len();
+            let to_idx = match names.entry(to) {
+                std::collections::hash_map::Entry::Occupied(slot) => *slot.get(),
+                std::collections::hash_map::Entry::Vacant(slot) => {
+                    adj_list.push(Vec::new());
+                    node_cap.push(to.as_bytes()[0].is_ascii_uppercase());
+                    *slot.insert(next_idx)
+                }
+            };
             if from_idx != 0 && to_idx != 1 {
                 adj_list[from_idx].push(to_idx);
             }
@@ -31,37 +37,47 @@ impl Graph {
         Self { adj_list, node_cap }
     }
 
-    fn path_count<const CYCLE: bool>(&self, curr: usize, visited: &mut Vec<bool>) -> u32 {
+    fn path_count(
+        &self,
+        curr: usize,
+        visited_mask: u32,
+        twice_allowed: bool,
+        mem: &mut std::collections::HashMap<(usize, u32, bool), u32>,
+    ) -> u32 {
         if curr == 0 {
             return 1;
         }
-        let mut count = 0;
-        for &neighbor in &self.adj_list[curr] {
-            match (visited[neighbor], self.node_cap[neighbor]) {
-                (false, _) | (_, true) => {
-                    visited[neighbor] = true;
-                    count += self.path_count::<CYCLE>(neighbor, visited);
-                    visited[neighbor] = false;
-                }
-                (true, false) => {
-                    if CYCLE {
-                        count += self.path_count::<false>(neighbor, visited);
-                    }
-                }
-            }
+        if let Some(count) = mem.get(&(curr, visited_mask, twice_allowed)) {
+            return *count;
         }
+        let mut next_visited_mask = visited_mask;
+        let mut next_twice_allowed = twice_allowed;
+        if !self.node_cap[curr] {
+            if visited_mask & (1 << curr) != 0 {
+                if !twice_allowed {
+                    return 0;
+                }
+                next_twice_allowed = false;
+            }
+            next_visited_mask |= 1 << curr;
+        }
+        let mut count = 0;
+        for neighbor in &self.adj_list[curr] {
+            count += self.path_count(*neighbor, next_visited_mask, next_twice_allowed, mem);
+        }
+        mem.insert((curr, visited_mask, twice_allowed), count);
         count
     }
 }
 
 pub fn part1(input: &str) -> u32 {
     let graph = Graph::new(input);
-    graph.path_count::<false>(1, &mut vec![false; graph.adj_list.len()])
+    graph.path_count(1, 0, false, &mut Default::default())
 }
 
 pub fn part2(input: &str) -> u32 {
     let graph = Graph::new(input);
-    graph.path_count::<true>(1, &mut vec![false; graph.adj_list.len()])
+    graph.path_count(1, 0, true, &mut Default::default())
 }
 
 #[cfg(test)]
